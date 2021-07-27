2aa4e7c243f779befb8ca84f7abb751b8f4593a8
Usenmfon <usenmfonuko@gmail.com>
2021-07-27 23:25:36 +0100

    completed todos

--- src/tasks/TodoItem.js
+++ src/tasks/TodoItem.js
@@ -1,13 +1,19 @@
 import React from "react"
 
 function TodoItem(props){
+    const CompletedStyle = {
+        fontStyle: "italic",
+        textDecoration: "line-through",
+        color: "#cdcdcd"
+    }
+
     return(
         <div className="todo-item">
             <input type="checkbox" 
             checked={props.item.completed}
             onChange={() => props.handleChange(props.item.id)}
             />
-            <p>Today's Task: {props.item.todo}</p>
+            <p style={props.item.completed ? CompletedStyle : null}>Today's Task: {props.item.todo}</p>
         </div>
     )
 }
